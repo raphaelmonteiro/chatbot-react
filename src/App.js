b41d3e0624003d816acd4b3657af6bb372831134
Raphael Monteiro <raphael.monteiro@superaremedia.com.br>
2018-08-22 16:12:52 -0300

    Adicionado redux no projeto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,12 +12,14 @@ import Chatbot from './components/Chatbot';
 class App extends Component {
   render() {
     return (
-      <div className="conteudo">
-        <Header />
-        <Home />
-        <Chatbot />
-        <Footer />
-      </div>
+      <Provider store={store}>
+        <div className="conteudo">
+          <Header />
+          <Home />
+          <Chatbot />
+          <Footer />
+        </div>
+      </Provider>
     );
   }
 }
